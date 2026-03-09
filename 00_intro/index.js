982176af5da6a79672d7e6b6805341220891c5d3
jordan h <jharrison1@qa.com>
2026-03-09 11:31:31 +0000

    init commit

---
+++ 00_intro/index.js
@@ -0,0 +1,43 @@
+let num = 21;
+
+let sunny = false;
+
+let name = "Jordan";
+let name2 = 'Cameron';
+let name3 = `JB`;
+
+let title = "grocer's";
+
+let myVar;
+// ctrl + / to create comments
+// console.log -> JS print
+console.log(myVar);
+
+let myNull = null;
+console.log(myNull);
+
+// camelCase -> capitalise the first letter 
+//              of every word from the second
+const myName = "Jordan Harison";
+// myName = "Jordan 2";
+
+// strings
+
+// concatenation
+
+let firstName = "Jordan";
+let lastName = "Harrison";
+
+let fullName = "Jordan" + " " + "Harrison";
+
+
+// interpolation
+
+let fullName2 = `${firstName} ${lastName}`;
+
+let age = 12;
+let job = "trainer";
+
+const greeting = "Hello! My name is " + name + " and I am a " + age + " years old " + job;
+const greeting2 = `Hello! My name is ${name} and I am a ${age} years old ${job}`;
+
