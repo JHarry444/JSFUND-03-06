60b190af564254e5e78f240652de2d0c1799c601
jordan h <jharrison1@qa.com>
2026-03-12 10:22:58 +0000

    modules

---
+++ 11_Modules/index.js
@@ -0,0 +1,27 @@
+import myCalc, { add, subtract, divide, multiply } from "./calc.js";
+
+document.querySelectorAll("#calcSection > button").forEach(button => {
+    button.addEventListener("click", function (event) {
+        const operation = event.target.textContent;
+
+        const firstNum = parseFloat(document.getElementById("firstNum").value);
+        const secondNum = parseFloat(document.getElementById("secondNum").value);
+        const output = document.getElementById("output");
+
+        switch (operation) {
+            case '+':
+                output.value = add(firstNum, secondNum);
+                break;
+            case '-':
+                output.value = subtract(firstNum, secondNum);
+                break;
+            case '*':
+                output.value = multiply(firstNum, secondNum);
+                break;
+            case '/':
+                output.value = divide(firstNum, secondNum);
+                break;
+
+        }
+    })
+});
