2c5b4e654f186e493b4fc2df009b6106e5515a4d
jordan h <jharrison1@qa.com>
2026-03-12 14:00:57 +0000

    replaces ''

--- 13_Node/webpack.config.js
+++ 13_Node/webpack.config.js
@@ -2,14 +2,14 @@ const path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: path.resolve(__dirname, "src/index.js"),
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js"
     },
     devServer: {
         static: {
-            directory: path.resolve(__dirname, 'dist')
+            directory: path.resolve(__dirname, "dist")
         },
         open: true,
         hot: true,
